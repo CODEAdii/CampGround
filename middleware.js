726104a76c15e06d6b0ccbb07d8e8afbe0ee0397
Aditya Maurya <87596186+CODEAdii@users.noreply.github.com>
2023-12-25 17:35:00 +0545

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,85 +1,57 @@
- // Assuming schemas.js is in the same directory
- const { campgroundSchema,reviewSchema } = require('./schemas');
-const ExpressError=require('./utils/ExpressError');
-// Add this line to import the Campground model
-const Campground = require('./models/campground'); 
-const Review = require('./models/review');
+const express = require('express');
+const router = express.Router();
+const campgrounds=require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const {isLoggedIn,isAuthor,validateCampground,validateReview}=require('../middleware');
+const Campground = require('../models/campground');
 
-
-module.exports.storeReturnTo = (req, res, next) => {
-    if (req.session.returnTo) {
-        res.locals.returnTo = req.session.returnTo;
-    }
-    next();
-}
-
-
-
-module.exports.isLoggedIn=(req,res,next)=>{
- //we use passport helper method i.e is Authencated----to show login user access
-  if(!req.isAuthenticated()){
- req.session.returnTo=req.originalUrl
-    req.flash('error','You must be register to Yelp Camp');
-    return res.redirect('/login');
-  }
-  next();
-}
-
-
-
-// Middleware for validating campground data
-module.exports.validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-}
+// // Display all campgrounds
+// router.get('/', catchAsync(campgrounds.index));
+// // Show form to create a new campground
+// router.get('/new', isLoggedIn,campgrounds.renderNewForm);
+// // // Handle form submission to create a new campground
+// // router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+// //   const { error } = campgroundSchema.validate(req.body);
+// //   if (error) {
+// //     const msg = error.details.map(el => el.message).join(',');
+// //     throw new ExpressError(msg, 400);
+// //   }
+// //   const campground = new Campground(req.body.campground);
+// //   await campground.save();
+// //   req.flash('success','New Campground Added!!');
+// //   res.redirect(`/campgrounds/${campground._id}`);
+// // }));
+// // Handle form submission to create a new campground
+// router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+// // Show details of a specific campground
+// router.get('/:id', catchAsync(campgrounds.showCampground));
+// // Show form to edit a campground
+// router.get('/:id/edit',isLoggedIn,isAuthor,catchAsync(campgrounds.renderEditCampground ))
+// // Handle form submission to edit a campground
+// router.put('/:id',isLoggedIn,isAuthor,validateCampground, catchAsync(campgrounds.updateCampground));
+// // Delete a campground
+// router.delete('/:id',isLoggedIn, isAuthor,catchAsync(campgrounds.deleteCampground));
 
 
 
+//-----------------fancy way to restructure our routesss-----
 
-module.exports.isAuthor = async (req, res, next) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
+router.route('/')
+  .get(catchAsync(campgrounds.index))
+  .post( isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-  // Check if user is logged in
-  if (!req.isAuthenticated()) {
-    req.flash('error', 'You need to be logged in to do that!');
-    return res.redirect('/login'); // Redirect to the login page if not logged in
-  }
-
-  // Check if the user is the author of the campground
-  if (!campground.author.equals(req.user._id)) {
-    req.flash('error', 'You do not have permission to do that!');
-    return res.redirect(`/campgrounds/${id}`);
-  }
-
-  next();
-};
-
-module.exports.isReviewAuthor = async (req, res, next) => {
-  const { id, reviewId } = req.params;
-  const review = await Review.findById(reviewId);
-
-  if (!review || !review.author.equals(req.user._id)) {
-    req.flash('error', 'You do not have permission to do that!');
-    return res.redirect(`/campgrounds/${id}`);
-  }
-
-  next();
-};
+router.get('/new', isLoggedIn,campgrounds.renderNewForm);
 
 
+ router.route('/:id')  
+    .get( catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn,isAuthor,validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor,catchAsync(campgrounds.deleteCampground))
 
-module.exports.validateReview = (req,res,next) => {
-  const{error} = reviewSchema.validate(req.body);
-    if (error) {
-      const msg = error.details.map(el => el.message).join(',')
-      throw new ExpressError(msg, 400)
-    } else {
-      next();
-    }
-}
+   
+ router.get('/:id/edit',isLoggedIn,isAuthor,catchAsync(campgrounds.renderEditCampground ))
+
+
+ 
+module.exports = router;
+ 
