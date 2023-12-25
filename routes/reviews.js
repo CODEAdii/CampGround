f65a2200b1617eeebd017aceeebb5b7ea76ed0e8
Aditya Maurya <87596186+CODEAdii@users.noreply.github.com>
2023-12-25 17:36:15 +0545

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,48 +1,21 @@
-const express=require('express');
-const router=express.Router({mergeParams:true});
+const express = require('express');
+const router = express.Router({mergeParams:true});
+const reviews = require('../controllers/reviews');
 //since the id is not define in the review path we use mergeParams:true in oder to use params id
-
-const Campground=require('../models/campground');
-const Review=require('../models/review');
+const {validateReview,isLoggedIn,isReviewAuthor} = require('../middleware');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
 
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
-const{reviewSchema}=require('../schemas.js')
+
+//create review
+router.post('/',isLoggedIn, validateReview,catchAsync(reviews.createReview))
 
 
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-  }
-router.post('/', validateReview,catchAsync(async(req, res)=>{
-    // res.send("You made it");
-  const campground = await Campground.findById(req.params.id);
-  const review = new Review(req.body.review);
-  review.campground = campground; // Make sure the association is set
-  campground.reviews.push(review);
-  await review.save();
-  await campground.save();
-  req.flash('success','Review Added!!');
-
-  res.redirect(`/campgrounds/${campground._id}`);
-  
-  }))
-  //delete reviews
-router.delete('/:reviewId',catchAsync(async(req,res)=>{
-  const {id,reviewId}= req.params;
-  await Campground.findByIdAndUpdate(id, {$pull:{reviews: reviewId} });
-  await Review.findByIdAndDelete(reviewId);
-
-  req.flash('success','Review Deleted!!');
-  res.redirect(`/campgrounds/${id}`);
-}))
+//delete reviews
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor,catchAsync(reviews.deleteReview))
   
 
 
